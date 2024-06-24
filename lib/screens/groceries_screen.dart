db5f39f008c231cc0a88b3baefe3fa2f52759a8f
Prabesh Narsingh Kunwar <prabeshkunwar12@gmail.com>
2024-06-24 16:02:49 -0300

    Home screen uses list of grocery items created instead of dummy items

--- lib/screens/groceries_screen.dart
+++ lib/screens/groceries_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_app/data/dummuy_items.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
 import 'package:shopping_list_app/screens/new_item_screen.dart';
 import 'package:shopping_list_app/widgets/grocery_item_widget.dart';
 
@@ -11,10 +11,20 @@ class GroceriesScreen extends StatefulWidget {
 }
 
 class _GroceriesScreenState extends State<GroceriesScreen> {
-  void _addItem() {
-    Navigator.of(context).push(MaterialPageRoute(
-      builder: (ctx) => const NewItemScreen(),
-    ));
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItemScreen(),
+      ),
+    );
+
+    if (newItem == null) return;
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
   @override
@@ -30,9 +40,9 @@ class _GroceriesScreenState extends State<GroceriesScreen> {
         ],
       ),
       body: ListView.builder(
-        itemCount: groceryItems.length,
+        itemCount: _groceryItems.length,
         itemBuilder: (ctx, index) =>
-            GroceryItemWidget(groceryItem: groceryItems[index]),
+            GroceryItemWidget(groceryItem: _groceryItems[index]),
       ),
     );
   }
--- lib/screens/new_item_screen.dart
+++ lib/screens/new_item_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
 
 class NewItemScreen extends StatefulWidget {
   const NewItemScreen({super.key});
@@ -20,9 +21,12 @@ class _NewItemScreenState extends State<NewItemScreen> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      print(_enteredName);
-      print(_enteredQuantity);
-      print(_selectedCategory);
+      Navigator.of(context).pop(GroceryItem(
+        id: DateTime.now().toString(),
+        name: _enteredName,
+        quantity: _enteredQuantity,
+        category: _selectedCategory,
+      ));
     }
   }
 
